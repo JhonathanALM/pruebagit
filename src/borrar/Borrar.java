293e8ad8e2b9de85f509297238245e56876fc12b
JhonathanALM <jalechon@espe.edu.ec>
2018-10-11 18:16:32 -0500

    parte3

--- src/borrar/Borrar.java
+++ src/borrar/Borrar.java
@@ -10,6 +10,7 @@ public class Borrar {
     public static void main(String[] args) {
         System.out.println(" hola git");
         System.out.println("Prueba 2 de github");
+        System.out.println("Parte 3");
         
        
     }
